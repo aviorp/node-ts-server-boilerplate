e6c66f640a0590e3f9656c5c75df3779a4190be3
Avior Peretz <avior.peretz@aquasec.com>
2022-10-25 00:54:37 +0300

    align the boilerplate according to the web boilerplate behavior

--- src/BL/Auth.ts
+++ src/BL/Auth.ts
@@ -1,16 +1,24 @@
-import { genSaltSync, hashSync } from "bcrypt-nodejs";
+import { genSaltSync, hashSync } from "bcryptjs";
 import { UserI } from "../interfaces";
 import AuthService from "../services/AuthService";
 import UserService from "../services/UserService";
-const salt = genSaltSync(10);
 
-export const register = (user: UserI) => {
-  const hashedPassword = hashSync(user.password!, salt);
-  const newUser = { ...user, password: hashedPassword };
-  return AuthService.register(newUser);
-};
+/**
+ *  This Class is responsible for the business logic of the authentication.
+ * @class Auth Business Logic Class for the authentication.
+ */
+class AuthBL {
+  async register(user: UserI) {
+    const salt = await genSaltSync(10);
+    const hashedPassword = await hashSync(user.password!, salt);
+    const newUser = { ...user, password: hashedPassword };
+    return AuthService.register(newUser);
+  }
+  async login(username: string, passwordToCompare: string) {
+    const user = await UserService.getByUsername(username);
 
-export const login = async (email: string, password: string) => {
-  const user = await UserService.getUserByEmail(email);
-  return await AuthService.login(password, user);
-};
+    return await AuthService.login(user, passwordToCompare);
+  }
+}
+
+export default new AuthBL();
