691f1f77826038579d7667b8048a340ab7bbb4c4
Avior Peretz <avior.peretz@aquasec.com>
2023-03-28 13:31:11 +0300

    COMMIT CHECK

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,6 @@ import settings from "./utils/settings";
 /**
  * This file is responsible for the init of the backend.
  * @class HttpService - Application bootstrap file
+ * @method start - Starts the server
  */
 HttpService.start(config.port!, settings);
