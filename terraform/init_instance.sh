d207f903a2b3390c5d67c47e2e560b8fabe2f708
Avior Peretz <avior.peretz@aquasec.com>
2023-05-16 16:56:27 +0300

    🐛 fix(init_instance.sh): remove unnecessary files and commands ✨ feat(init_instance.sh): add docker and docker-compose installation and app.yaml file creation The unnecessary files and commands were removed from the script. Docker and docker-compose were installed and enabled. An app.yaml file was created to define the server and watchtower services. The server service runs on port 80 and connects to a MongoDB database. The watchtower service is responsible for updating the server container.

--- terraform/init_instance.sh
+++ terraform/init_instance.sh
@@ -3,14 +3,37 @@
 # install and update apt-get
 sudo apt-get update -y
 
+# install and update docker
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
 
 # install and update docker-compose
 sudo apt-get install -y docker-compose
 
-# copy the docker-compose.yml file to the instance
-cp ./server.yaml "$(pwd)"
-cp ./.env "$(pwd)"
+# write app.yaml
+cat > app.yaml << EOF
+version: "3.4" # optional since v1.27.0
+services:
+  server:
+    container_name: server
+    image: aviorp/node-ts-server
+    environment:
+      - PORT=3300
+      - JWT_SECRET=secret
+      - DB_URI="mongodb+srv://admin:TagO16AAivmbS7Wx@cluster0.mgw5eqj.mongodb.net/TEST_DB"
+      - NODE_ENV=DEV
+    restart: always
+    ports:
+      - 80:3300
+  watchtower:
+    container_name: watchtower
+    restart: always
+    image: containrrr/watchtower
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+    command: --cleanup --interval 30
+EOF
 
-
-# run docker-compose
-sudo docker-compose -f server.yaml up -d
